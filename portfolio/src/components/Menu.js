6e5f55cd208dc15d4a542960843e28b2877520fc
Margaux Doisy <margauxdoisy@gmail.com>
2025-01-20 22:52:28 +0100

    refonte css desktop mobile

--- portfolio/src/components/Menu.js
+++ portfolio/src/components/Menu.js
@@ -1,19 +1,32 @@
-import React from 'react';
-import '../components/Apropos';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/components/Menu.css';
 
 const Menu = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
     return (
-        <div className='naviguation'>
-            <ul>
-            
-                <li><Link to="/portfolio">Portfolio</Link></li>
-                <li><Link to="/monHistoire">Mon histoire</Link></li>
-                <li><Link to="/cvonline"> Mon parcours</Link></li>
-                <li><Link to="/contact">Contact </Link></li>
-                
-            </ul>
+        <div className="menu-container">
+            {/* Icône de menu hamburger */}
+            <div className="hamburger" onClick={toggleMenu}>
+                <div className="bar"></div>
+                <div className="bar"></div>
+                <div className="bar"></div>
+            </div>
+
+            {/* Navigation */}
+            <div className={`naviguation ${isOpen ? 'open' : ''}`}>
+                <ul>
+                    <li><Link to="/portfolio">Portfolio</Link></li>
+                    <li><Link to="/monHistoire">Mon histoire</Link></li>
+                    <li><Link to="/cvonline">Mon parcours</Link></li>
+                    <li><Link to="/contact">Contact</Link></li>
+                </ul>
+            </div>
         </div>
     );
 };
